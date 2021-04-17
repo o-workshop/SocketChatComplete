4e662aea602ac64e13b08c4985e61fc42551741f
Ousmane Traore <hello@ousmane.me>
2021-04-17 15:13:11 -0400

    Update tk chat

--- full-client.py
+++ full-client.py
@@ -2,24 +2,25 @@ import socket
 import threading
 import sys
 
+
 class Client:
     def __init__(self):
-        self.host = '165.22.177.88'
+        self.host = 'chat.ousmane.me'
         self.port = 1234
         self.username = ''
         self.create_connection()
 
     def create_connection(self):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         try:
-            self.socket.connect((self.host, self.port))
+            self.client.connect((self.host, self.port))
 
         except:
             print("Couldn't connect to server")
 
         self.username = input('Enter username: ')
-        self.socket.send(self.username.encode())
+        self.client.send(self.username.encode())
 
         message_handler = threading.Thread(target=self.handle_messages, args=())
         message_handler.start()
@@ -29,16 +30,16 @@ class Client:
 
     def handle_messages(self):
         while True:
-            print(self.socket.recv(1204).decode())
+            print(self.client.recv(1204).decode())
 
     def input_handler(self):
         while True:
             msg = input('>')
             if str(msg) == "QUIT":
-                self.socket.shutdown(socket.SHUT_RDWR)
-                self.socket.close()
+                self.client.shutdown(socket.SHUT_RDWR)
+                self.client.close()
                 sys.exit()
-            self.socket.send(str(msg).encode())
+            self.client.send(str(msg).encode())
 
 
 client = Client()
